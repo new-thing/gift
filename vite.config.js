cf3da6a52fb12dbadb41efb67440620a8bf31db3
new-thing <kanikadrive.00@gmail.com>
2025-08-21 01:23:19 +0530

    gift-card

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: "/anniversary-gift/",   
   plugins: [react()],
+    base: "/gift/",
+    
 })
