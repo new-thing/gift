0717348dbdda94d2c9bda942e6c456a59c57a9b8
new-thing <kanikadrive.00@gmail.com>
2025-08-21 10:18:16 +0530

    gift-card-gift

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom/client"; 
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/gift">
+    <App />
+  </BrowserRouter>
 );
